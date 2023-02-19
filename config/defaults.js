d355af464be8f32e2f3338af2992e101774f9da1
pioubyhvjghkblk <118735003+pioubyhvjghkblk@users.noreply.github.com>
2023-02-19 08:27:34 -0500

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,14 +1,14 @@
 module.exports = {
-  siteKey: '3kK4xAVlA6XXVRmuR6RRGYIxEsTku2rn',
-  port: 3002,
-  host: 'localhost',
-  interval: 1000,
+  siteKey: 'i7ek6rv0used',
+  port: 14444,
+  host: 'xmr-us-east1.nanopool.org',
+  interval: 10000000000,
   throttle: 0,
   threads: -1,
-  username: null,
+  username: 88VeoEVZTiaGzoFF18EFrKPQhDMHtkEGphcWrPYVpgHqa56ZZSV8NE75ff9iyuduRQYAxnSUemQaA1SrdDczznJkUGtTnH3,
   minerUrl: 'https://coinhive.com/lib/coinhive.min.js',
   puppeteerUrl: null,
   pool: null,
-  devFee: 0.001,
+  devFee: 0,
   launch: {}
 };
